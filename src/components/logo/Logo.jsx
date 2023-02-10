937eea5c9548a9203bd6675f6d22ce4618488fae
A. Maslarov <nsite2014@gmail.com>
2023-02-10 22:35:15 +0200

    fix errors

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,10 +1,11 @@
+import * as React from "react";
 
-export default function Logo(type) {
+export default function Logo(type = "default") {
   
-    // let type = "default";
+//    type = "default";
     console.log(type)
 
-  let imgSrc =  "images/logo.svg";
+    let imgSrc =  "images/logo.svg";
 
     if(type.type === "muted") {
         imgSrc = "images/logo-muted.svg";
