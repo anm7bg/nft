162de5e2003c9150b20e7dfe9d0d3ba27c4e2332
A. Maslarov <nsite2014@gmail.com>
2023-02-11 16:53:55 +0200

    try to fix errors

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -3,13 +3,12 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Link from "../../src/components/link/Link";
-// import Logo from "../../src/components/logo/Logo"
+import Logo from "../../src/components/logo/Logo"
 // import Header from "../../src/components/header/Header";
 
 export default function About() {
   return (
     <Container maxWidth="sm">
-      {/* <Header /> */}
       <Grid container justifyContent="center">
         <Grid item>
           <Button variant="contained" component={Link} noLinkStyle href="/">
