55b6238cbeb8e9a71aac17037e65b63bda9c7046
A. Maslarov <nsite2014@gmail.com>
2023-02-11 16:20:38 +0200

    fix errors

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+import Link from "../link/Link";
+import styles from "../Example/Example.module.scss";
+import classNames from "classnames";
+
+
+
+export default function Header() {
+    return(
+        <div className={classNames(styles.wrapper)}>
+        <Container className={classNames(styles.container)} maxWidth="xl">
+
+        </Container>
+      </div>
+    );
+}
