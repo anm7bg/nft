50c14f7f487fa4939478593c7aa75c60c2829c70
A. Maslarov <nsite2014@gmail.com>
2023-02-13 15:34:44 +0200

    Revert "Merge pull request #3 from anm7bg/dev"
    This reverts commit eada62e74518303a2c167d7e0c3ee98414a18566.

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -4,11 +4,14 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Link from "../../src/components/link/Link";
 import Logo from "../../src/components/logo/Logo"
-import Header from "../../src/components/header/Header";
 
 export default function About() {
   return (
     <Container maxWidth="sm">
+<<<<<<< HEAD
+=======
+   
+>>>>>>> parent of eada62e... Merge pull request #3 from anm7bg/dev
       <Grid container justifyContent="center">
         <Grid item>
           <Button variant="contained" component={Link} noLinkStyle href="/">
