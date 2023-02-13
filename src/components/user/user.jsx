f845a75eef559015eeea172a7eaaf0b3cc1d4ea4
A. Maslarov <nsite2014@gmail.com>
2023-02-13 16:57:34 +0200

    Added text-size in rem

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -5,7 +5,7 @@ export default function User({name="", info="", avatar="", size=55, verified=fal
     return(
         <div className="user">
             <Avatar />
-            <div>
+            <div className="userInfo">
                 <p className="name">{name}</p>
                 <p className="info">{info}</p>
             </div>
