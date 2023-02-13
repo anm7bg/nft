6c07e17ee751ce81778165b86eb97635e5056383
A. Maslarov <nsite2014@gmail.com>
2023-02-13 14:28:47 +0200

    fix errors

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,10 +1,5 @@
 export default function Avatar({verified = false, size={width:"90px", height:"90px"}, url = "/images/avatar.png"}) {
 
-    // size = {
-    //     width: "90px",
-    //     height: "90px"
-    //     // background: "red"
-    // }
 
     let verifiedBadgeDisplay = {
         display: "none"
@@ -19,11 +14,8 @@ export default function Avatar({verified = false, size={width:"90px", height:"90
 
     return(
         <div className="avatar" style={ size }>
-            <img src={ url } className="image" width={size.width} alt="avatar"></img>
+            <img src={ url } className="image" width="100%" alt="avatar"></img>
             <img src="/images/verified.svg" style={ verifiedBadgeDisplay }  className="badge" alt="badge" />
         </div>
     );
 }
-// Avatar.defaultProps = {
-//     size:{width:"90px", height: "90px"}
-// }
