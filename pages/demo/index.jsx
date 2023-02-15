d2380221ce50f23c6a6a15b407bbf6291c96c20a
A. Maslarov <nsite2014@gmail.com>
2023-02-15 20:27:53 +0200

    fix errors

--- pages/demo/index.jsx
+++ pages/demo/index.jsx
@@ -20,7 +20,7 @@ export default function About() {
           </Button>
         </Grid>
       </Grid>
-      <Card />
+      <Card likes="1000000" price={100} />
     </Container>
   );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,19 +3,21 @@ import { CardContent, CardMedia, Chip, Card as MuiCard, CardActions } from '@mui
 import Avatar from '../avatar/Avatar';
 import millify from "millify";
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import styles from "./Card.module.scss";
+
 
 export default function Card( {url, verified, mediaUrl, name, price, currency, likes=0} ) {
     // let label = 
 
     return(
-        <MuiCard className="card">
+        <MuiCard className={styles.card}>
             <Avatar url={url} verified={verified} />
             <CardMedia className='media'>
                 image={mediaUrl}
             </CardMedia>
             <CardContent>
                 <h2 className='title'>{name}</h2>
-                <p className='price'>{price}{currency}</p>
+                <p className='price'>~{price}{currency}</p>
             </CardContent>
             
             <Chip icon={<FavoriteIcon />} className="likes" label={millify(likes)} variant="outlined" />
