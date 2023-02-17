750b247992df3855fe865c0ee74a1a623cd7de02
A. Maslarov <nsite2014@gmail.com>
2023-02-17 13:08:47 +0200

    Added Trending component

---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,31 @@
+import { Container, Grid } from "@mui/material";
+
+export default function Trending(name, url, verified, mediaUrl, price, curency) {
+
+    return(
+        <Container>
+            <div><h1>Trendung</h1></div>
+            <div>
+            <FormControl fullWidth>
+                <InputLabel id="simple-select-label">Age</InputLabel>
+                <Select
+                    labelId="simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="This week"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={"one"}>Ten</MenuItem>
+                    <MenuItem value={"two"}>Twenty</MenuItem>
+                    <MenuItem value={"three"}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+            </div>
+            <Grid container>
+                <Grid item>
+                    <Card />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
