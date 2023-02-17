ee39267e94e2c3e26d5e7e33aeb221e66d9c792c
A. Maslarov <nsite2014@gmail.com>
2023-02-18 01:15:12 +0200

    fix errors

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -12,7 +12,7 @@ export default function Card( {url, verified, mediaUrl, name, price, currency, l
         <MuiCard className={styles.card}>
             <Avatar className={styles.avatar} url={url} verified={verified} />
             <img className={styles.media} src={mediaUrl}></img>
-                <h2 className='title'>{name}</h2>
+                <h2 className={styles.title}>{name}</h2>
                 <p className='price'>~{price} {currency}</p>
             <Chip icon={<FavoriteIcon />} className="likes" label={millify(likes)} variant="outlined" />
         </MuiCard>
