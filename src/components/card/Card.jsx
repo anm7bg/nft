d97df19041311765a5ca299c0693659740a87ab7
A. Maslarov <nsite2014@gmail.com>
2023-02-18 19:17:28 +0200

    fix errors

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,11 +5,20 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import styles from "./Card.module.scss";
 
 
-export default function Card( {title, url, verified, mediaUrl, price, currency, likes=0} ) {
+export default function Card( {
+    title,
+    user = {
+        avatar:{url},
+        verified: true,
+    }, 
+    mediaUrl, 
+    price, 
+    currency, 
+    likes=0} ) {
 
     return(
         <MuiCard className={styles.card}>
-            <Avatar className={styles.avatar} url={url} verified={verified} />
+            <Avatar className={styles.avatar} url={user.avatar.url} verified={user.verified} />
             <img className={styles.media} src={mediaUrl}></img>
             <h2 className={styles.title}>{title}</h2>
             <p className='price'>~{price} {currency}</p>
