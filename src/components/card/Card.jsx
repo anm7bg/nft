e1a2b919a0e13252428db3ff67ce77993e00e3b4
A. Maslarov <nsite2014@gmail.com>
2023-02-18 16:23:31 +0200

    fix errrors

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,4 @@
 import { CardContent, CardMedia, Chip, Card as MuiCard, CardActions } from '@mui/material';
-// import {} from '@mui/material/Card';
 import Avatar from '../avatar/Avatar';
 import millify from "millify";
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -12,7 +11,7 @@ export default function Card( {title, url, verified, mediaUrl, price, currency, 
         <MuiCard className={styles.card}>
             <Avatar className={styles.avatar} url={url} verified={verified} />
             <img className={styles.media} src={mediaUrl}></img>
-            <div className={styles.title}>{title}</div>
+            <h2 className={styles.title}>{title}</h2>
             <p className='price'>~{price} {currency}</p>
             <Chip icon={<FavoriteIcon />} className="likes" label={millify(likes)} variant="outlined" />
         </MuiCard>
