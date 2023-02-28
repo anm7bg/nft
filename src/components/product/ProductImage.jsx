60c99a2a34cbf7666459d4416a8143537c988429
A. Maslarov <nsite2014@gmail.com>
2023-02-28 16:56:04 +0200

    Fix errors

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -2,6 +2,7 @@ import styles from "./ProductImage.module.scss"
 
 export default function ProductImage({url, alt}) {
 
+ 
     return(
         <div className={styles["product-image"]}>
             <img className={image} src={url} alt={alt} />
