4c58bb244e6981efeb1942f715399fba773c3aff
A. Maslarov <nsite2014@gmail.com>
2023-02-28 18:09:25 +0200

    Added PoductTile Component

---
+++ src/components/product/ProductInfoTitle.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import styles from "./ProductInfoTitle.module.css"
+
+export default function ProductInfoTitle({ text }) {
+
+    return(
+        <div className={styles["product-info-title"]}>
+            <Typography className={styles.title}>
+                {text}
+            </Typography>
+        </div>
+    );
+}
