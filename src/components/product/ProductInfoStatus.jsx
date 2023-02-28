d3e670d9f738268b7020e0113cd480e04b35b74a
A. Maslarov <nsite2014@gmail.com>
2023-02-28 23:01:08 +0200

    Fix Errors

--- src/components/product/ProductInfoStatus.jsx
+++ src/components/product/ProductInfoStatus.jsx
@@ -1,3 +1,5 @@
+import { Chip } from "@mui/material";
+import LiveIcon from "@mui/icons-material/FiberManualRecord";
 import styles from "./ProductInfoStatus.module.scss";
 
 export default function ProductInfoStatus({ }) {
@@ -5,9 +7,9 @@ export default function ProductInfoStatus({ }) {
     return(
         <div className={styles["product-info-status"]}>
                     <Chip 
-                        icon={<FavoriteIcon />} 
+                        icon={<LiveIcon />} 
                         className={styles.status} 
-                        label="status"
+                        label={"Live".toUpperCase()}
                         variant="outlined"
                     />
         </div>
