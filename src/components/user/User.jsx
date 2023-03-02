0e237d4fbe2f99a3ad4be9a71e308b0980745e57
A. Maslarov <nsite2014@gmail.com>
2023-03-03 00:33:12 +0200

    Fix errors

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,11 +1,11 @@
 import Avatar from "../avatar/Avatar";
 import styles from "./User.module.scss";
 
-export default function User( {name="", info="", avatar="", size=55, verified=false} ) {
+export default function User( {avatar, name, info, size=55, verified} ) {
 
     return(
         <div className={styles.user}>
-            <Avatar size={size} />
+            <Avatar size={size} verified url={avatar}/>
             <div className={styles.userInfo}>
                 <p className={styles.name}>{name}</p>
                 <p className={styles.info}>{info}</p>
