be2d391cd94367fd5f8aae65a650388a22f43468
A. Maslarov <nsite2014@gmail.com>
2023-03-02 23:23:40 +0200

    Fix errors

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -1,9 +1,10 @@
-import * as React from 'react';
+// import * as React from 'react';
 import { TabPanel, TabContext, TabList } from "@mui/lab";
 import { Box, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import styles from "./ProductTabs.module.scss";
 import User from '../user/User';
 import { parseISO, formatDistance } from "date-fns";
+import { useState } from "react";
 
 export default function ProductTabs({ text, bids=[] }) {
 
@@ -46,7 +47,7 @@ export default function ProductTabs({ text, bids=[] }) {
     //     },
     // ]
 
-    const [value, setValue] = React.useState('1');
+    const [value, setValue] = useState('1');
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -64,36 +65,33 @@ export default function ProductTabs({ text, bids=[] }) {
                     {text}
                 </TabPanel>
                 <TabPanel value="2">
-                    <TableContainer component="div">
                         <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
+                            {/* <TableHead>
                                 <TableRow>
                                     <TableCell>User</TableCell>
                                 </TableRow>
-                            </TableHead>
+                            </TableHead> */}
                             <TableBody>
                                 {bids.map((row, i) => (
                                     <TableRow
                                     key={i}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                     className={`styles[table-row-${i}] ${styles["table-row"]}`}
                                     // className={`table-row-${i} ${styles["table-row"]}`}
                                     >
-                                        <TableCell component="th" scope="row">
+                                        <TableCell>
                                             <User {...row.user} />
                                         </TableCell>
-                                        <TableCell component="th" scope="row">
+                                        <TableCell>
                                             {row.amount}
                                         </TableCell>
-                                        <TableCell component="th" scope="row">
+                                        <TableCell>
                                             {formatDistance(parseISO(row.date), Date.now())}
                                         </TableCell>
-
                                     </TableRow>
                                 ))}
                             </TableBody>
                         </Table>
-                    </TableContainer>
                 </TabPanel>
             </TabContext>
         </div>
